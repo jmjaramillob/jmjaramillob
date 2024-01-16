6084dc1bcbdc240061d3d3e39f14e2b1d2b07602
jmjaramillob <jmjaramillob@gmail.com>
2024-01-15 22:48:30 -0500

    Se agregó el crud de acciones, criterios y politicas

--- apps/multipolv1/views.py
+++ apps/multipolv1/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import TemplateView, ListView, UpdateView, CreateView, DeleteView, DetailView
-from apps.multipolv1.forms import EstudioMultipolForm
+from django.contrib.sitemaps.views import index
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView, DetailView
+from apps.multipolv1.forms import EstudioMultipolForm, Accion, Criterio, Politica
 from django.contrib import messages
+from django.http import JsonResponse
+import json
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
 
-from apps.multipolv1.models import EstudioMultipol
+from apps.multipolv1.forms import AccionForm, CriterioForm, PoliticaForm, EstudioMultipolForm
+from apps.multipolv1.models import EstudioMultipol, EvaluacionCriterioAccion, EvaluacionCriterioPolitica
 from apps.proyecto.models import Proyecto, Tecnica
 from apps.proyecto.views import contexto_mensajes, obtener_tipo_usuario_estudio
 
@@ -20,9 +26,6 @@ class DetalleEstudio(DetailView):
     estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk'])
     context['proyecto'] = estudio.idProyecto
     context['usuario'] = obtener_tipo_usuario_estudio(self.request, estudio.id, 5)
-    
-    
-
     return context
 
 class CrearEstudio(CreateView):
@@ -86,7 +89,6 @@ class UpdateEstudio(UpdateView):
 
 
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
 
 
 @csrf_exempt
@@ -101,4 +103,288 @@ def delete_estudio(request, *args, **kwargs):
        return JsonResponse({'status': 'Invalid request'}, status=400)
     return JsonResponse({'status': 'Invalid request'}, status=400)
 
+
 ### Gestion de acciones del estudio ############################################################
+class CreateAccion(CreateView):
+  model = Accion
+  form_class = AccionForm
+  template_name = 'multipol/acciones/add_accion.html'
+
+  def get_context_data(self, **kwargs):
+    context = super(CreateAccion, self).get_context_data(**kwargs)
+    estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk'])
+    context['estudio'] = estudio
+    #self.actualizar_informe(estudio)
+
+    context.update(contexto_mensajes(self.request))
+    return context
+
+
+class ListAccion(ListView):
+  model = Accion
+  template_name = 'multipol/acciones/accion_list.html'
+  context_object_name = 'acciones'
+
+  def get_context_data(self, **kwargs):
+    context = super(ListAccion, self).get_context_data(**kwargs)
+    
+    estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk'])
+    context['estudio'] = estudio
+    context['acciones'] = Accion.objects.filter(estudio=estudio.id)
+    print(context['acciones'])
+    context.update(contexto_mensajes(self.request))
+    return context
+
+
+class EditAccion(UpdateView):
+  model = Accion
+  form_class = AccionForm
+  template_name = 'multipol/acciones/edit_accion.html'
+  #success_url = reverse_lazy('multipol:lista_accion')
+
+  def get_context_data(self, **kwargs):
+    context = super(EditAccion, self).get_context_data(**kwargs)
+    #estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk_estudio'])
+    #context['estudio'] = estudio
+    #self.actualizar_informe(estudio)
+
+    context.update(contexto_mensajes(self.request))
+    return context
+
+
+class DeleteAccion(DeleteView):
+  model = Accion
+  #success_url = reverse_lazy('multipol:list_estudio')
+
+  def post(self, request, *args, **kwargs):
+    data = dict()
+    if request.is_ajax():
+
+      accion = json.loads(request.POST['content'])
+      for i in accion:
+        accion_delete = get_object_or_404(Accion, pk=i['id_accion'])
+        accion_delete.delete()
+        acciones = Accion.objects.all()
+        data['html_data'] = render_to_string(
+          'multipol/includes/accion_list_table.html', {'acciones': acciones})
+    else:
+
+      context = {}
+      data['html_form'] = render_to_string(
+        'multipol/includes/accion_confirm_delete.html', context,
+        request=request)
+    return JsonResponse(data)
+  
+
+### Gestion de criterios del estudio ############################################################
+
+class CreateCriterio(CreateView):
+  model = Criterio
+  form_class = CriterioForm
+  template_name = 'multipol/criterios/add_criterio.html'
+
+  def get_context_data(self, **kwargs):
+    context = super(CreateCriterio, self).get_context_data(**kwargs)
+    estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk'])
+    context['estudio'] = estudio
+    #self.actualizar_informe(estudio)
+
+    context.update(contexto_mensajes(self.request))
+    return context
+
+class ListCriterio(ListView):
+  model = Criterio
+  template_name = 'multipol/criterios/criterio_list.html'
+  context_object_name = 'criterios'
+
+  def get_context_data(self, **kwargs):
+    context = super(ListCriterio, self).get_context_data(**kwargs)
+    estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk'])
+    context['estudio'] = estudio
+    context['criterios'] = Criterio.objects.filter(estudio=estudio.id)
+    #self.actualizar_informe(estudio)
+
+    context.update(contexto_mensajes(self.request))
+    return context
+
+class EditCriterio(UpdateView):
+  model = Criterio
+  form_class = CriterioForm
+  template_name = 'multipol/criterios/edit_criterio.html'
+
+  def get_context_data(self, **kwargs):
+    context = super(EditCriterio, self).get_context_data(**kwargs)
+    print(self.request)
+    #estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk_estudio'])
+    #context['estudio'] = estudio
+    #self.actualizar_informe(estudio)
+
+    context.update(contexto_mensajes(self.request))
+    return context
+
+class DeleteCriterio(DeleteView):
+  model = Criterio
+  success_url = reverse_lazy('multipol:lista_criterio')
+
+  def post(self, request, *args, **kwargs):
+    data = dict()
+    if request.is_ajax():
+
+      criterio = json.loads(request.POST['content'])
+      for i in criterio:
+        criterio_delete = get_object_or_404(Criterio, pk=i['id_criterio'])
+        criterio_delete.delete()
+        criterios = Criterio.objects.all()
+        data['html_data'] = render_to_string(
+          'multipol/includes/criterio_list_table.html',
+          {'criterios': criterios}
+        )
+    else:
+
+      context = {}
+      data['html_form'] = render_to_string(
+        'multipol/includes/criterio_confirm_delete.html', context,
+        request=request)
+    return JsonResponse(data)
+
+### Gestion de políticas del estudio ############################################################
+
+class CreatePolitica(CreateView):
+  model = Politica
+  form_class = PoliticaForm
+  template_name = 'multipol/politicas/add_politica.html'
+
+  def get_context_data(self, **kwargs):
+    context = super(CreatePolitica, self).get_context_data(**kwargs)
+    estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk'])
+    context['estudio'] = estudio
+    #self.actualizar_informe(estudio)
+
+    context.update(contexto_mensajes(self.request))
+    return context
+
+class ListPolitica(ListView):
+  model = Politica
+  template_name = 'multipol/politicas/politica_list.html'
+  context_object_name = 'politicas'
+
+  def get_context_data(self, **kwargs):
+    context = super(ListPolitica, self).get_context_data(**kwargs)
+    estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk'])
+    context['estudio'] = estudio
+    context['politicas'] = Politica.objects.filter(estudio=estudio.id)
+    #self.actualizar_informe(estudio)
+
+    context.update(contexto_mensajes(self.request))
+    return context
+
+class EditPolitica(UpdateView):
+  model = Politica
+  form_class = PoliticaForm
+  template_name = 'multipol/politicas/edit_politica.html'
+
+  def get_context_data(self, **kwargs):
+    context = super(EditPolitica, self).get_context_data(**kwargs)
+    #estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk'])
+    #context['estudio'] = estudio
+    #self.actualizar_informe(estudio)
+
+    context.update(contexto_mensajes(self.request))
+    return context
+
+
+class DeletePolitica(DeleteView):
+  model = Politica
+  success_url = reverse_lazy('multipol:lista_politica')
+
+  def post(self, request, *args, **kwargs):
+    data = dict()
+    estudio = get_object_or_404(EstudioMultipol, id=self.kwargs['pk'])
+    if request.is_ajax():
+
+      politica = json.loads(request.POST['content'])
+      for i in politica:
+        politica_delete = get_object_or_404(Politica, pk=i['id_politica'])
+        politica_delete.delete()
+        politicas = Politica.objects.all()
+        data['html_data'] = render_to_string(
+          'multipol/includes/politica_list_table.html',
+          {'politicas': politicas})
+    else:
+
+      context = {}
+      data['html_form'] = render_to_string(
+        'multipol/includes/politica_confirm_delete.html', context,
+        request=request)
+    return JsonResponse(data)
+  
+
+###### Gestión de las evaluaciones para los resultados de estudio #####################################################
+
+
+def evaluacion_criterio_accion(request, pk):
+  estudio = get_object_or_404(EstudioMultipol, id=pk)
+  #evalt = EvaluacionTotalCA.objects.filter(estudio=estudio.id)
+  #if evalt.exist():
+  #  evalt = EvaluacionTotalCA(estudio=estudio)
+  acciones = Accion.objects.filter(estudio=estudio.id)
+  criterios = Criterio.objects.filter(estudio=estudio.id)
+  contexto = {}   
+
+  if request.method == 'POST' or request.is_ajax():
+    data = json.loads(request.POST['content'])
+    for i in data:
+      accion = Accion.objects.get(pk=i['accion'])
+      criterio = Criterio.objects.get(pk=i['criterio'])
+      puntuacion = i['puntuacion']
+      evaluacion_criterio_accion = EvaluacionCriterioAccion(estudio=estudio, accion=accion, criterio=criterio, puntuacion=puntuacion)
+    evaluacion_criterio_accion.save()
+    return index(request)
+
+
+    self.actualizar_informe(estudio)
+
+  context = {'acciones': acciones, 'criterios': criterios, 'estudio': estudio}
+  return render(request, 'multipol/evaluaciones/evaluacion_criterio_accion.html', context)
+
+
+def evaluacion_criterio_politica(request, pk):
+  estudio = get_object_or_404(EstudioMultipol, id=pk)
+  politicas = Politica.objects.filter(estudio=estudio.id)
+  criterios = Criterio.objects.filter(estudio=estudio.id)
+  experto = request.user
+  #self.actualizar_informe(estudio)
+
+  context = {'politicas': politicas, 'criterios': criterios, 'estudio': estudio}
+  # evaluacionCP = EvaluacionCP
+  # if EvaluacionCP.objects.exists():
+  #   evaluacionCP = EvaluacionCP.objects.get(0)
+  #   valueCP = evaluacionCP.valoracionCP
+  #   context = {'politicas': politicas, 'criterios': criterios,
+  #              'Valoraciones': valueCP}
+  if request.method == 'POST' or request.is_ajax():
+    data = json.loads(request.POST['content'])
+    for i in data:
+      politica = Politica.objects.get(pk=i['politica'])
+      criterio = Criterio.objects.get(pk=i['criterio'])
+      puntuacion = i['puntuacion']
+      evaluacion_criterio_politica = EvaluacionCriterioPolitica(
+        estudio=estudio,
+        politica=politica,
+        criterio=criterio,
+        puntuacion=puntuacion,
+        opinion="aabc",
+        idExperto=experto
+      )
+      evaluacion_criterio_politica.save()
+    return index(request)
+
+  return render(request, 'multipol/evaluaciones/evaluacion_criterio_politica.html', context)
+
+
+def llenar_matriz_evaluacionCA(request, pk):
+    #consenso = verificar_consenso(request, idEstudio)
+    evaluacionesCA = EvaluacionCriterioAccion.objects.filter(estudio=pk).values()
+    evaluacionesCA = list(evaluacionesCA)
+    print(evaluacionesCA)
+    return JsonResponse({'evaluacionesCA': evaluacionesCA})
